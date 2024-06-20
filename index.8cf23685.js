5e56b7e9691ebd5c29b0c7c2e228cf29a7622f40
metrakon <metrakon@proton.me>
2024-06-21 00:37:25 +0300

    make build

---
+++ index.8cf23685.js
@@ -0,0 +1,2 @@
+var c=document.querySelectorAll(".gallery__panel");c.forEach(function(e){e.addEventListener("click",function(){c.forEach(function(c){c.classList.remove("active")}),e.classList.add("active")})});
+//# sourceMappingURL=index.8cf23685.js.map
